0a968417fd9e0419fac5581d57b6cc60a844d04c
Mario Cabrera <yankeescab@gmail.com>
2021-03-06 22:18:03 -0500

    Finalized additions needed to have the password generater function properly

--- Develop/script.js
+++ Develop/script.js
@@ -14,9 +14,8 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
-
-function generatePassword() {
+var inputArray = [];
+var passArray = [];
 
 var lowerCharacters = [
   "q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"  
@@ -34,10 +33,70 @@ var specialCharacters = [
   "!", '"', "#", "$", "%", "&", "'", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"
 ];
 
-var numOfCharacters = prompt ("How long do you want your password? (between 8 and 128)");
-var numberCharacters = confirm ("Do you want you password to contain numbers?");
-var upperCharacters = confirm ("Do yo want your password to contain uppercase numbers?");
-var lowerCharacters = confirm ("Do you want your password to contain lower case characters?");
-var specialCharacters = confirm ("Do you want your password to contain special characters?")
+function askForOptions() {
+  var passLength = prompt ("How long do you want your password? (between 8 and 128)");
+  
+  if (passLength === "") {
+    alert("Please enter a number")
+    return;
+  } 
+  passLength = parseInt(passLength)
+  console.log(passLength)
 
+  if (isNaN(passLength) === true){
+    alert("Input must be a number")
+    return;
+  }
+
+  if (passLength <8 || passLength >128) {
+    alert("Please input a number between 8 and 128")
+    return;
+  }
+  var numberBoolean = confirm ("Do you want you password to contain numbers?");
+ 
+  var upperBoolean = confirm ("Do yo want your password to contain uppercase numbers?");
+  
+  var lowerBoolean = confirm ("Do you want your password to contain lower case characters?");
+ 
+  var specialBoolean = confirm ("Do you want your password to contain special characters?");
+ 
+  
+  if (numberBoolean === false && upperBoolean === false && lowerBoolean === false && specialBoolean === false){
+    alert("Please select one character type")
+    return;
+  }
+  var options = {
+    passLength, numberBoolean, upperBoolean, lowerBoolean, specialBoolean
+  }
+  console.log(options)
+  return options;
 }
+
+function generatePassword() {
+  var options = askForOptions()
+  console.log(options)
+  if(options.lowerBoolean === true){
+    inputArray=inputArray.concat(lowerCharacters);
+  }
+  if(options.upperBoolean === true){
+    inputArray=inputArray.concat(upperCharacters);
+  }
+  if(options.specialBoolean === true){
+    inputArray=inputArray.concat(specialCharacters);
+  }
+  if(options.numberBoolean === true){
+    inputArray=inputArray.concat(numberCharacters);
+  }
+  console.log(inputArray)
+  for (var i = 0; i < options.passLength; i++){
+    var index = Math.floor(Math.random() * inputArray.length)
+    console.log(index)
+
+    var passdigit = inputArray[index]
+    passArray.push(passdigit)
+    console.log(passdigit)
+  }
+  return passArray.join("")
+
+
+};
